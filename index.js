746f588d30aaef141404ccc1571d2a3fe1bebe2c
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-02-14 21:03:31 +0200

    updated

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import express from 'express'
+import axios from 'axios'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app = express()
+const router = express.Router()
+const port = +process.env.PORT || 4000
+const baseURL = ''
